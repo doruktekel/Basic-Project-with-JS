52f933fc014e0e7fc832b1e772fec0339e5c177c
DORUK TEKEL <95101042+drktklcll@users.noreply.github.com>
2023-07-19 00:08:31 +0300

    JS WITH API

---
+++ DadJokesGenerator/app.js
@@ -0,0 +1,38 @@
+const btnEl = document.getElementById("btn");
+const jokeEl = document.getElementById("joke");
+
+const apiKey = "3KvJH+jZcBwTTZnE/Z5eGw==85wcgH1oJfKAwzDy";
+
+const options = {
+  method: "GET",
+  headers: { "X-Api-Key": apiKey },
+};
+
+const apiURL = "https://api.api-ninjas.com/v1/dadjokes?limit=1";
+
+async function getJoke() {
+    try {
+        jokeEl.innerText = "Updating...";
+        btnEl.disabled = true;
+        btnEl.innerText = "Loading...";
+      
+        const response = await fetch(apiURL, options);
+        const data = await response.json();
+      
+        btnEl.disabled = false;
+        btnEl.innerText = "Tell me a joke";
+      
+        jokeEl.innerText = data[0].joke;
+        
+    } catch (error) {
+        jokeEl.innerText = "An Error Happened , Try Again Later";
+        btnEl.disabled = false;
+        btnEl.innerText = "Tell me a joke";
+
+        console.log(error)
+    }
+
+ 
+}
+
+btnEl.addEventListener("click", getJoke);
